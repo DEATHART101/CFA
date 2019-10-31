f259e392167b8f6017ffc51e05289170c58f3ea2
薛爱德华 <xueeduard@xueaidehuadeMacBook-Air.local>
2019-11-01 00:08:37 +0800

    init

---
+++ CFA/CFA.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CFA
+{
+    public class CFAClient
+    {
+        #region Defines
+
+        public enum SendControlTypes
+        {
+            None,
+            Arrival,
+            Ordered,
+            Both
+        };
+
+        private const int COMMMON_BUFFER_LENGTH = 2000;
+
+        private class CFATunnel
+        {
+            #region Defines
+
+            private const int MESSAGE_QUEUE_LENGTH = 2000;
+            private const int MESSAGE_RESEND_INTERVAL = 2;  // ms
+
+            #endregion
+
+            private int m_
+            private byte[][] m_messages;
+
+            public CFATunnel()
+            {
+                m_messages = new byte[MESSAGE_QUEUE_LENGTH][];
+            }
+        }
+
+        #endregion
+
+        private UdpClient m_updClient;
+        private byte[] m_buffer;
+
+
+        public CFAClient()
+        {
+        }
+
+        #region Interfaces
+
+        public void Send(byte[] dgram, int bytes, IPEndPoint endPoint, SendControlTypes sendControlType)
+        {
+            
+
+            switch (sendControlType)
+            {
+                case SendControlTypes.None:
+                    break;
+                case SendControlTypes.Arrival:
+                    break;
+                case SendControlTypes.Ordered:
+                    break;
+                case SendControlTypes.Both:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private void ClearBuffer()
+        {
+            for (int i = 0; i < COMMMON_BUFFER_LENGTH; i++)
+            {
+                m_buffer[i] = 0;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ CFA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<IPEndPoint, int> test = new Dictionary<IPEndPoint, int>() ;
+
+        IPEndPoint a = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 77);
+        IPEndPoint b = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 77);
+
+        test.Add(a, 19);
+        test.Add(b, 20);
+
+        Console.WriteLine(test.Count);
+    }
+}
